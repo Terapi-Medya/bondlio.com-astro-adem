7e8c4ef038199e4fdeba6345fbdcbb6b44975b89
Terapi Media Digital Marketing & Software <info@terapimedya.com>
2026-06-26 16:03:00 +0300

    lots of stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), mdx()],
+
+  i18n: {
+    defaultLocale: 'tr',
+    locales: ['tr', 'en'],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
 
   vite: {
     plugins: [tailwindcss()]
